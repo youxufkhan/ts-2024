60c6e4d12efacd1bbd37af90d769a0fd9b8bb65d
Yousuf Khan <m.youxuf@gmail.com>
2024-04-28 17:19:58 +0500

    primitive and object types

--- 013 - Types.ts
+++ 013 - Types.ts
@@ -1,5 +1,12 @@
 // 013 Type -> Easy way to refer to different properties + functions that a value has
 
+
+//             [Primitive Types] -> [number] [boolean] [void] [undefined] 
+// [Types]   ->                     [string] [symbol] [null]
+//             [Object Types]    -> [functions] [arrays]
+//                                  [classes] [objects]
+
+
 //const today: Date
 const today = new Date();
 today.getHours();
