8f09e7ccb2b2c70dd730bfd33d68482f155f2d20
Yousuf Khan <m.youxuf@gmail.com>
2024-04-28 17:20:11 +0500

    type annotations and type inference

---
+++ 015 - Type Annotations-Type Inference.ts
@@ -0,0 +1,13 @@
+//  015
+// -- Type Annotations + Type Inference
+// 
+// 1. Variables
+// 2. Functions
+// 3. Objects
+
+// Type Annotations -> Code we add to tell TypeScript what type of value a variable will refer to
+// - We tell typescript the type
+
+// Type Inference -> Typescript tries to figure out what type of value a variable refers to
+// - Typescript guesses the type
+ 
