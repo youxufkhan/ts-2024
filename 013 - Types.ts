ed45fbdaf0f1c2f66627d9ebd532596a5b6af52f
Yousuf Khan <m.youxuf@gmail.com>
2024-04-28 17:09:13 +0500

    013 Types

---
+++ 013 - Types.ts
@@ -0,0 +1,25 @@
+// 013 Type -> Easy way to refer to different properties + functions that a value has
+
+//const today: Date
+const today = new Date();
+today.getHours();
+
+// ---
+// const person: {
+//     age: number;
+// }
+const person = {
+    age: 20
+};
+
+// --
+class Color {
+
+}
+// const red: Color
+const red = new Color();
+
+//Where do we use types -> Everywhere!
+
+
+
